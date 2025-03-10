6528567665579131181e6db057e7f40d9805e7b8
Amir Shaikh <154251113+Amir5117@users.noreply.github.com>
2025-03-10 11:39:06 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,46 @@
+class Book {
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrowBook() {
+        if (!isBorrowed) {
+            isBorrowed = true;
+            System.out.println("You borrowed: " + title);
+        } else {
+            System.out.println("Sorry, this book is already borrowed.");
+        }
+    }
+
+    public void returnBook() {
+        if (isBorrowed) {
+            isBorrowed = false;
+            System.out.println("You returned: " + title);
+        } else {
+            System.out.println("This book was not borrowed.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + (isBorrowed ? " [Borrowed]" : " [Available]");
+    }
+}
